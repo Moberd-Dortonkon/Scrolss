36aa7ff739b0ab82d69d42e3fc1a6d2c6181e8e1
Moberd-Dortonkon <Moberd-Dortonkon@yandex.ru>
2018-05-18 00:37:01 +0300

    Moberd design master handbook

--- app/src/main/java/com/example/koolguy/scroll/HandBookFragment.java
+++ app/src/main/java/com/example/koolguy/scroll/HandBookFragment.java
@@ -2,59 +2,95 @@ package com.example.koolguy.scroll;
 
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.app.ListFragment;
 import android.content.Context;
 import android.content.res.Resources;
+import android.media.Image;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.ScrollView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HandBookFragment extends ListFragment {
-
+public class HandBookFragment extends Fragment {
+    MyMap map;
     View view;
 
     public HandBookFragment() {
         // Required empty public constructor
     }
-    public static interface HandbookListener
-    {
+
+    public static interface HandbookListener {
         void handBookClick(int position);
 
     }
+
     HandbookListener listener;
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        listener =(HandbookListener)activity;
+        listener = (HandbookListener) activity;
 
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_hand_book, container, false);
-        Resources res=getResources();
-        String[] HandBook = res.getStringArray(R.array.HandBook);
-        ArrayAdapter<String> BookAdapter=new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1,HandBook);
-        setListAdapter(BookAdapter);
-        return super.onCreateView(inflater, container, savedInstanceState); //ListFragment должен это возвращать
-         }
+//        map = new MyMap(    ,inflater.getContext() );
+        ScrollView sv = (ScrollView) view.findViewById(R.id.handbook);
+        ImageButton schBtn=(ImageButton)view.findViewById(R.id.schdBtn);
+        schBtn.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TeamList2 mapFragment = new TeamList2();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.frames, mapFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        }));
 
-    @Override
+        ImageButton mapBtn=(ImageButton)view.findViewById(R.id.mapBtn);
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                map.makeMap();
 
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        listener.handBookClick(position);
+            }
+        });
 
-
+        ImageButton helpBtn=(ImageButton) view.findViewById(R.id.helpBtn);
+        helpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirstHelpFragment mapFragment = new FirstHelpFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.frames, mapFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+        return view;
     }
+
 }
+
+//    @Override
+//
+//    public void onListItemClick(ListView l, View v, int position, long id) {
+//        super.onListItemClick(l, v, position, id);
+//        listener.handBookClick(position);
+//
+//
+//    }
+//}
