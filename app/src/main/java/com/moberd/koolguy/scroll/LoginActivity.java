812f09457374eac09c8268b2156f8443bf3d0ed2
Moberd-Dortonkon <Moberd-Dortonkon@yandex.ru>
2018-06-13 23:25:57 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/moberd/koolguy/scroll/CreateVolonteer.java
    #	app/src/main/java/com/moberd/koolguy/scroll/InDevelopment.java
    #	app/src/main/java/com/moberd/koolguy/scroll/LeaderGroup.java
    #	app/src/main/java/com/moberd/koolguy/scroll/MainActivity.java
    #	app/src/main/java/com/moberd/koolguy/scroll/MyMap.java
    #	app/src/main/java/com/moberd/koolguy/scroll/Tools/ExpandableAdpter.java
    #	app/src/main/java/com/moberd/koolguy/scroll/Tools/ExpandableListListener.java
    #	app/src/main/java/com/moberd/koolguy/scroll/Tools/ShowGroupForVolonteerAdapter.java
    #	app/src/main/java/com/moberd/koolguy/scroll/groups/GroupCreator.java
    #	app/src/main/java/com/moberd/koolguy/scroll/serverInterfaces/ServerGetMyInformation.java

--- app/src/main/java/com/moberd/koolguy/scroll/LoginActivity.java
+++ app/src/main/java/com/moberd/koolguy/scroll/LoginActivity.java
@@ -3,13 +3,8 @@ package com.moberd.koolguy.scroll;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
